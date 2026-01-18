c59a3b39cb16ef829ecb60e3c550ee75072fa65e
www <97720090+wwwx3@users.noreply.github.com>
2026-01-18 17:45:30 +0700

    Create 4-1-2564 Soln.cpp

---
+++ POSN_COM Camp1-2564/4-1-2564/4-1-2564 Soln.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int MONTHS[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+int main() {
+    int m, d;
+    if (!(cin >> m >> d)) return 0;
+
+    // 0 = Sunday
+    d = (d+11)%7;
+
+    // Move month-by-month to reach August (8)
+    while (m < 8) {
+        d = (d + (MONTHS[m-1] % 7)) % 7;
+        m++;
+    }
+    while (m > 8) {
+        d = (d - (MONTHS[m-2] % 7)) % 7;
+        if (d < 0) d += 7;
+        m--;
+    }
+
+    cout << d << '\n';
+    return 0;
+}
